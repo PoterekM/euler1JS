b753d2795331451da96993b0b3ae2dcd7fb88b00
Michelle Poterek <poterekm@gmail.com>
2017-08-20 12:26:37 -0700

    modify code for correct answer to be less than 1000

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 function naturalNumbers(number) {
   var total = 0;
-  for (var i = 1; i <= 1000; i ++) {
-    if ((i % 5) === 0 || (i % 3) === 0) {
+  for (var i = 1; i < 1000; i ++) {
+    if ((i % 5) === 0) {
+      total += (i);
+    } else if ((i % 3) === 0) {
       total += (i);
     }
   }
