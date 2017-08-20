2251e6cc22cfb4dbaa223b12cc3f80d1ae16513a
Michelle Poterek <poterekm@gmail.com>
2017-08-20 12:15:56 -0700

    write natural number function that produces correct answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+function naturalNumbers(number) {
+  var total = 0;
+  for (var i = 1; i <= 1000; i ++) {
+    if ((i % 5) === 0) {
+      total += (i);
+    } else if ((i % 3) === 0) {
+      total += (i);
+    }
+  }
+  return total;
+}
+
 $(document).ready(function() {
-  $("#answer").text("boop");
+  var total = naturalNumbers(total);
+  $("#answer").text(total);
 });
