8a46e81b2a2fa77068dfde6c02d55bedc0152091
Michelle Poterek <poterekm@gmail.com>
2017-08-20 12:19:09 -0700

    refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 function naturalNumbers(number) {
   var total = 0;
   for (var i = 1; i <= 1000; i ++) {
-    if ((i % 5) === 0) {
-      total += (i);
-    } else if ((i % 3) === 0) {
+    if ((i % 5) === 0 || (i % 3) === 0) {
       total += (i);
     }
   }
